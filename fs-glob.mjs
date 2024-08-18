ddda38bb504c3be1a6d6b2b8036ed1527ac66ce5
azu <azuciao@gmail.com>
2024-08-18 09:35:05 +0900

    Update .github/workflows/test.yml+4

--- fs-glob.mjs
+++ fs-glob.mjs
@@ -1,7 +1,5 @@
 import { glob as fsPromisesGlob } from "node:fs/promises"
 import path from "node:path";
-import { glob as nodeGlob } from "glob";
-import fastGlob from "fast-glob";
 
 const cwd = process.cwd();
 {
@@ -24,8 +22,8 @@ const cwd = process.cwd();
         },
         withFileTypes: true,
     });
-    const childFilesIgnoreWithGlob = fsPromisesGlob(["fixtures/**/*", "!**/fixtures/root/child-*/**/*"],{
-        withFileTypes:true,
+    const childFilesIgnoreWithGlob = fsPromisesGlob(["fixtures/**/*", "!**/fixtures/root/child-*/**/*"], {
+        withFileTypes: true,
     });
     console.log("# fs/promises glob");
     console.log("------------------");
@@ -39,55 +37,4 @@ const cwd = process.cwd();
     console.log("childFilesIgnoreWithGlob", (await Array.fromAsync(childFilesIgnore, dirent => {
         return path.relative(cwd, path.join(dirent.parentPath, dirent.name))
     })));
-}
-
-// node-glob
-{
-    // all
-    const allFiles = await nodeGlob("fixtures/**/*");
-    // txt
-    const txtFiles = await nodeGlob("fixtures/**/*.txt");
-    // root file path
-    const rootFilePath = await nodeGlob("fixtures/root/index.txt");
-    // child files
-    const childFiles = await nodeGlob("fixtures/root/child-*/**/*");
-    // ignore child-y
-    const childFilesIgnore = await nodeGlob(["fixtures/**/*"], {
-        ignore: ["fixtures/root/child-*/**/*"]
-    });
-    const childFilesIgnoreWithGlob = await nodeGlob(["fixtures/**/*", "!fixtures/root/child-*/**/*"]);
-    console.log("# node-glob");
-    console.log("-----------");
-    console.log("allFiles", allFiles);
-    console.log("txtFiles", txtFiles);
-    console.log("rootFilePath", rootFilePath);
-    console.log("childFiles", childFiles);
-    console.log("childFilesIgnore", childFilesIgnore);
-    console.log("childFilesIgnoreWithGlob", childFilesIgnoreWithGlob);
-    
-}
-
-// fast-glob
-{
-    // all
-    const allFiles = await fastGlob("fixtures/**/*");
-    // txt
-    const txtFiles = await fastGlob("fixtures/**/*.txt");
-    // root file path
-    const rootFilePath = await fastGlob("fixtures/root/index.txt");
-    // child files
-    const childFiles = await fastGlob("fixtures/root/child-*/**/*");
-    // ignore child-y
-    const childFilesIgnore = await fastGlob(["fixtures/**/*"], {
-        ignore: ["!fixtures/root/child-y/**/*"]
-    });
-    const childFilesIgnoreWithGlob = await fastGlob(["fixtures/**/*", "!fixtures/root/child-y/**/*"]);
-    console.log("# fast-glob");
-    console.log("------------");
-    console.log("allFiles", allFiles);
-    console.log("txtFiles", txtFiles);
-    console.log("rootFilePath", rootFilePath);
-    console.log("childFiles", childFiles);
-    console.log("childFilesIgnore", childFilesIgnore);
-    console.log("childFilesIgnoreWithGlob", childFilesIgnoreWithGlob);
 }
